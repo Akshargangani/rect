0ea5b899d68e85e112738c237e9bcbd13740ed5a
your name <your-email@example.com>
2024-12-04 15:20:43 +0530

    first commit

---
+++ test-app/src/App.jsx
@@ -0,0 +1,226 @@
+import { useState } from 'react'
+
+import './style.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+       <div class="f1" >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SEARCH &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CONTACT US</div>
+
+<div>
+      <div className="f2"><img src="logo (1).png" alt="Logo" /></div>
+      <div className="f3">Shop Products</div>
+      <div className="f4">How to Wash Clothes</div>
+      <div className="f5">Washing Machine 101</div>
+      <div className="f6">About Ariel</div>
+      <div className="r1">TRUST ARIEL</div>
+      <div className="r2">RANGE OF DETERGENTS</div>
+      <div className="r3">to take care of all your</div>
+      <div className="r4">LAUNDRY NEEDS!</div>
+      <div className="aril"><img src="https://images.ctfassets.net/qozw5xyqia8b/5QoWhvM2gJtpoJfgI1aPUw/ece36602f2f3507dd4c64d3f2ea71170/KV_DT.png?fm=avif" className="ak" alt="Ariel image" /></div>
+      <div className="q1">DISCOVER</div>
+      <div className="q2">OUR BEST</div>
+      <div className="q3">PRODUCTS</div>
+      <button className="e1">Learn more</button>
+
+      <div className="bac">
+        <img src="https://images.ctfassets.net/qozw5xyqia8b/7IZntf11WWrhGetoerFfDl/631caac138ae5f227c1720bdd2a045eb/ariel_logo_big_resized.png?fm=avif" className="rt" alt="Ariel logo" />
+      </div>
+      <div className="gate">FIND THE</div>
+      <div className="some">RIGHT ARIEL PRODUCT FOR YOU</div>
+      <div className="boder"><img src="" alt="" /></div>
+      <div className="boder1"><img src="" alt="" /></div>
+      <div className="nem">Browse by type<br/></div>
+      <div className="nem1">PODs, Liquid, Powder, and more.</div>
+      <div className="img1"><img src="https://images.ctfassets.net/qozw5xyqia8b/17H5aWeEqwOME2mGTxzjAu/0e243a611bbac3a45107debb057748ac/Ariel_IN_module2_DT_411x417_202209291712_resized.png?fm=avif" alt="Product image" /></div>
+      <div className="text">Browse by need</div>
+      <div className="text1">Fully automatic washing machine, Front load washing machine, and more.</div>
+      <div className="img2"><img src="https://images.ctfassets.net/qozw5xyqia8b/4hvLCGTpG3Ah2TG2dq6Bj/31dd75396dda40d9d878a085dfb2c853/browse_by_needs.png?fm=avif" alt="Product image" /></div>
+
+      <div className="text2">#SHARETHELOAD</div>
+      <div className="text3">Everyone can master laundry</div>
+      <div className="img3"><img src="https://storage.googleapis.com/a1aa/image/rH0yohtWof11VaqtAQgyEtgXzbCNLMPf7fhtZ3h7siRQjeaPB.jpg" alt="Laundry image" /></div>
+      <button className="png">Learn more</button>
+
+      <div className="cloth"><img src="https://images.ctfassets.net/qozw5xyqia8b/5XYLTMoMuKWtVLtbS05OyL/a5252f8daae5cbb188ffa2f290cef8e5/spinner_mobile_bg_resized.png?fm=avif" alt="Cloth background" /></div>
+      <div className="text4">Stain removal guide<br/>Hair dye<br/>Ink<br/>Lipstick</div>
+      <div className="mak">Make-up</div>
+      <div className="meta">Mango<br/>Mould<br/>Nail polish<br/>Oil and grease<br/>Paint<br/>Pomegranate</div>
+      <button className="fix">Learn how to remove</button>
+      <div className="stain">STAIN</div>
+      <div className="can">Can't find the right stain?</div>
+      <div className="stape"></div>
+      <div className="id">FEATURED</div>
+      <div className="pe">PRODUCT</div>
+      <div className="mou">For expert clean.</div>
+      <div className="img4"><img src="https://images.ctfassets.net/qozw5xyqia8b/5Mqaek0I0N5PL9OvmjS4ot/1073c8d0e0c642cecd3652fd15134e22/Ariel_4-in-1_Pods_listing_image_152x204_20220928.png?fm=avif" alt="Ariel PODS image" /></div>
+      <div className="text5">Ariel Matic 4in1 PODS</div>
+      <button className="push">Shop Now</button>
+      <div className="star">★★★★★ 1 review</div>
+      <div className="text6">Ariel Matic Top Load<br/>&nbsp;washing Powder</div>
+      <div className="img5"><img src="https://images.ctfassets.net/qozw5xyqia8b/59BLf7i2raz1vJI3ZZQojC/389a13591dedf7688c03d5fc9d131bf9/04987176044426_C1C1_IN_152x204_220328.png?fm=avif" alt="Ariel powder image" /></div>
+      <button className="stare1">Shop Now</button>
+      <div className="star2">★★★★ 7 review</div>
+      <div className="img7"><img src="https://images.ctfassets.net/qozw5xyqia8b/4d4aMdrsOamDumsDT1thvA/11069684f878e0b884cad2a5e3a811f8/04987176051059_C1N1_IN_152x204_220328.png?fm=avif" alt="Ariel liquid image" /></div>
+      <div className="text7">Ariel Matic Front Load<br/>&nbsp;Liquid Detergent</div>
+      <button className="call">Shop Now</button>
+      <div className="star3">★★★ 41 review</div>
+      <div className="prode">View all products</div>
+      <div className="join">DISCOVER</div>
+      <div className="fast">ARIEL LAUNDRY TIPS</div>
+      <div className="second"><img src="https://images.ctfassets.net/qozw5xyqia8b/5W7wCoZyqfTGPlmi5PdODi/e4c9d71224b5ec84d94b53dfb47ec549/10504_ARIEL_Curry_Final-360x233.jpg?fm=avif" alt="Curry stain image" /></div>
+      <div className="don"></div>
+      <div className="text8">How to remove curry<br/>stains from clothes</div>
+      <div className="no">Although a nice curry dish, such as<br/>butter chicken, is fantastic to have on<br/>your plate, getting it on<br/>your clothes raises the question of how<br/>to remove turmeric stains.</div>
+
+
+      <div className="md">Read More</div>
+      <div className="error">
+        <img
+          src="https://images.ctfassets.net/qozw5xyqia8b/6umxRYguf6S7h9f6DnSk48/07ee5776a376c8022c2c73b3940d2af6/41943_ARIEL_Dye_Transfer_Final-360x233.jpg?fm=avif"
+          alt="Colour stain example"
+        />
+      </div>
+      <div className="none">How to remove colour stains from clothes</div>
+      <div className="de">
+        We all know the shock when you open the door of your washing machine and realise your white garments have now turned pink.
+      </div>
+      <div className="dr">Read More</div>
+
+      {/* Instagram Section */}
+      <div className="rang">CHECK OUT</div>
+      <div className="rang1">OUR #LAUNDRYGRAM</div>
+      <div className="te">
+        Get your daily dose of Ariel on Instagram. Browse, like, and follow us for some glow-rious content.
+      </div>
+      <div className="flex">
+        <div className="img10">
+          <img
+            src="https://images.ctfassets.net/qozw5xyqia8b/2IEZyzH4n19p2ruGjrnXJ4/59c2777f3f91ba40eadc65751f50a8d6/Ariel_IN_instagram_sharedtheload.jpg?fm=avif"
+            alt="Instagram post"
+            height="330px"
+            width="300px"
+          />
+        </div>
+        <div className="in">
+          <img
+            src="https://images.ctfassets.net/qozw5xyqia8b/MKNs1jeQmTpbyoZ2paP7A/17363b8a22f4d962e7c2fa37cc70b634/Ariel_IN_instagram_merry_christmas.jpg?fm=avif"
+            alt="Instagram post"
+            height="330px"
+            width="300px"
+          />
+        </div>
+        <div className="so">
+          <img
+            src="https://images.ctfassets.net/qozw5xyqia8b/4qxacXOl3304EVEKpCUERW/e4ab7d988601055f69589ce4c17a70d5/Ariel_IN_instagram_61_of_women.jpg?fm=avif"
+            alt="Instagram post"
+            height="330px"
+            width="300px"
+          />
+        </div>
+        <div className="jk">
+          <img
+            src="https://images.ctfassets.net/qozw5xyqia8b/5evLNFGW0eD1wdnnNxLsBU/97c416eb52131ad95a5995e7df5bb49a/Ariel_IN_instagram_we-ll-care-of-a-100-stains.jpg?fm=avif"
+            alt="Instagram post"
+            height="330px"
+            width="300px"
+          />
+        </div>
+        <div className="ji">
+          <img
+            src="https://images.ctfassets.net/qozw5xyqia8b/5n87qSNKjMHrUfWGskVdcz/2bfede697f4ef57ec7c4df2b39c78ac3/Ariel_IN_instagram_serving_spotless_clean.jpg?fm=avif"
+            alt="Instagram post"
+            height="330px"
+            width="300px"
+          />
+        </div>
+        <div className="lk">
+          <img
+            src="https://images.ctfassets.net/qozw5xyqia8b/2kQzdS6a7PW6xlrCZqGSM4/b361face50c3a28f4d61c3f827d57116/Ariel_IN_instagram_stains.jpg?fm=avif"
+            alt="Instagram post"
+            height="330px"
+            width="300px"
+          />
+        </div>
+        <div className="go">
+          <img
+            src="https://images.ctfassets.net/qozw5xyqia8b/4HhNRkaBlfviMZlLDsMkfO/14191b7a26671ac98feb8c1618479a26/Ariel_IN_instagram_top_stains.jpg?fm=avif"
+            alt="Instagram post"
+            height="330px"
+            width="300px"
+          />
+        </div>
+        <div className="ok">
+          <img
+            src="https://images.ctfassets.net/qozw5xyqia8b/51dVS07InLb6XByjyM5LAb/45938ac7db47fad9db6b65e2435342ee/Ariel_IN_instagram_this-women-s-day.jpg?fm=avif"
+            alt="Instagram post"
+            height="330px"
+            width="300px"
+          />
+        </div>
+      </div>
+      <div className="bio">
+        <button className="btn">Check out @Ariel.India</button>
+      </div>
+
+      {/* Footer Section */}
+      <div className="folser">
+        <div className="last1"></div>
+        <div className="last">Follow us</div>
+      </div>
+      <div className="finesh">
+        <div className="facbook">
+          <img
+            src="https://tse1.mm.bing.net/th?id=OIP.zaiIl16zqTKDuhYIxeop0wHaHa&pid=Api&P=0&h=180"
+            alt="Facebook logo"
+            height="50px"
+            width="80px"
+          />
+        </div>
+        <div className="insta">
+          <img
+            src="https://tse4.mm.bing.net/th?id=OIP.jIjxU37e3484oqVvsn71IQHaHa&pid=Api&P=0&h=180"
+            alt="Instagram logo"
+            height="50px"
+            width="80px"
+          />
+        </div>
+        <div className="one">
+          <img
+            src="https://tse1.mm.bing.net/th?id=OIP.0ZzsExBLkRl3AduCcB-irgHaEK&pid=Api&P=0&h=180"
+            alt="Twitter logo"
+            height="50px"
+            width="80px"
+          />
+        </div>
+        <div className="part">
+          <img
+            src="https://tse1.mm.bing.net/th?id=OIP.yas-ymRcurU0lXQtHneV8wHaHa&pid=Api&P=0&h=180"
+            alt="LinkedIn logo"
+            height="50px"
+            width="80px"
+          />
+        </div>
+      </div>
+
+      <div className="lasttxt"></div>
+      <div className="txt1">P&G family</div>
+      <div className="txt2">Terms & Conditions</div>
+      <div className="txt3">Privacy</div>
+      <div className="txt4">Ad Choices</div>
+      <div className="txt5">Sitemap</div>
+      <div className="next1">Your Privacy Choices</div>
+
+      <div className="fast2">Select Location:</div>
+      <div className="logo">India - English</div>
+      <div className="out">© 2024 Procter & Gamble.</div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
